34fdff9972eeb229057d9cc89202c7a9b8313096
SB <simon@burgr.io>
2023-05-30 11:59:51 +0200

    remove trailing space

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -4,7 +4,7 @@ param(
 )
 
 if (!$env:SCOOP_HOME)
-{ $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) 
+{ $env:SCOOP_HOME = Convert-Path (scoop prefix scoop)
 }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
