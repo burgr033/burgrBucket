da7e57c48ae93045c014463620eb1e3676afe7a0
SB <simon@burgr.io>
2023-05-31 08:48:23 +0200

    bugfix and modify pyshim

--- scripts/pyshim.ps1
+++ scripts/pyshim.ps1
@@ -1,7 +1,13 @@
-param($dir)
+param($dir,[switch]$noconsole)
+
+$pyex = "python"
+
+if ($noconsole){
+    $pyex = "pythonw"
+}
 
 ls "$dir" | ? name -match '^[^_].*?\.py$' | % {
     $n = $_.name -replace '\.py$', ''
     $ps = "$dir\$n.ps1"
-    "& python `"`$PSScriptRoot\$n.py`" @args" > $ps
+    "& pythonw `"`$PSScriptRoot\$n.py`" @args" > $ps
 }
